acb73228b12888ac0b7620963e727b4dbb896bf1
emillyCrisallys <crisallisnunes83@gmail.com>
2024-12-04 23:14:41 -0300

    Atualiza a melhor

--- src/main/java/br/grupointegrado/ecommerce/controller/FornecedorController.java
+++ src/main/java/br/grupointegrado/ecommerce/controller/FornecedorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/fornecedore")
+@RequestMapping("/fornecedor")
 public class FornecedorController {
 
     @Autowired
